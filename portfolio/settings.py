706ad53d742119cc5d98f6bbebe248bed2be3041
glasses-png <cchelangat16@gmail.com>
2020-07-10 11:04:19 +0300

    changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = '&9_2%$-k3^cy25p=pr-5^#8&_k_vek3-q#nk94-@)2%v@go(8='
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = Config ('DEBUG', True)
+DEBUG = True
 
 ALLOWED_HOSTS = '*'
 
